7d023e9dbf80de7e9de8402b96d0aeff751e0239
Leonardo <noreply@leoflo.me>
2025-07-22 13:19:27 +0200

    completata la versione 1

--- src/test.rs
+++ src/test.rs
@@ -25,24 +25,6 @@ fn test_mint_b14_z12_gus() {
 
 // Checking
 #[test]
-fn test_check_b10_z6_foo() {
-    let stamp = "1:10:250717:foo::vQjseDND+DnvuUxL:0000000000000000EV".to_string();
-    let bits = 10 as u32;
-    let resource = "foo".to_string();
-
-    assert!(super::check(&stamp, bits, 10, &resource).is_ok());
-}
-
-#[test]
-fn test_check_b14_z12_gus() {
-    let stamp = "1:14:250717155411:gus::aFYGS+0pBkqYLO4N:0000000004k3".to_string();
-    let bits = 13 as u32;
-    let resource = "gas".to_string();
-
-    assert!(super::check(&stamp, bits, 10, &resource).is_err());
-}
-
-#[test]
 fn test_check_b20_z10_bar() {
     let stamp = "1:20:2507171554:bar::368jVUGU0BHGDgUQ:00000000003HgE".to_string();
     let bits = 20 as u32;
