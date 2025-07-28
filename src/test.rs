fef0f6e1a86add4678325e8043b6af33b3eb0833
Leonardo <noreply@leoflo.me>
2025-07-28 20:47:57 +0200

    piccoli fix quà e la

--- src/test.rs
+++ src/test.rs
@@ -13,22 +13,12 @@ fn test_mint_b10_z6_foo() {
     debug_print(bits, &resource, &stamp);
 }
 
-#[test]
-fn test_mint_b14_z12_gus() {
-    let bits = 14 as u32;
-    let date_width = 12 as u8;
-    let resource = "gus".to_string();
-
-    let stamp = super::mint(bits, date_width, &resource, crate::MintStrategy::Naive);
-    debug_print(bits, &resource, &stamp);
-}
-
 // Checking
 #[test]
-fn test_check_b20_z10_bar() {
-    let stamp = "1:20:2507171554:bar::368jVUGU0BHGDgUQ:00000000003HgE".to_string();
-    let bits = 20 as u32;
-    let resource = "bar".to_string();
+fn test_check_b10_z6_foo() {
+    let stamp = "1:10:250728:foo::LNH6Qm0XVzjerqS1:00000000000000005G".to_string();
+    let bits = 10 as u32;
+    let resource = "fou".to_string();
 
     assert!(super::check(&stamp, bits, 1, &resource).is_err());
 }
