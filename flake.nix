dc44cb652930d1a178187a5f28ab00363175e148
Leonardo <noreply@leoflo.me>
2025-11-11 18:33:09 +0100

    flake aggiornato

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
+      packages = with pkgs; [
         cargo rustc rust-analyzer
       ];
 
