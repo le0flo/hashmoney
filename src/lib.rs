242a9260deeccce1115302e0ca03bcff04fa08a3
Leonardo <noreply@leoflo.me>
2026-04-16 15:42:45 +0200

    rispettate le linee guida fsf

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+/*
+Copyright (C) 2026 leoflo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //! You can either mint a stamp or parse it from a string, then check if it is valid against some given parameters.
 //! Only version 1 of the specification is implemented.
 //! The `wasm32-unknown-unknown` target is a first class citizen and fully supported with the `wasm` feature.
